4717cce2e168f4876972d255e6b8ee770f430476
chaozhouzhang <13760289294@139.com>
2020-03-27 01:56:56 +0800

    add check signature and aes

---
+++ NDK/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,19 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_android_stack_ndk_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello NDK.";
+    return env->NewStringUTF(hello.c_str());
+}
+
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_android_stack_ndk_MainActivity_plus(JNIEnv *env, jclass clazz, jint a, jint b) {
+    // TODO: implement plus()
+    jint sum = a+b;
+    return sum;
+}
---
+++ NDK/app/src/main/java/android/stack/ndk/MainActivity.java
@@ -0,0 +1,51 @@
+package android.stack.ndk;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author zhangchaozhou
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private final String str = "123abcABC*%!~#+_/中文测试";
+    private final String encode = "SkiDk/JC5F/BXKf/np7rWNub7ibxzYMjKwkQ7A6AqPw=";
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        tv.append(String.valueOf(plus(1, 99)));
+        tv.append("\n");
+
+        String secret = AESEncrypt.encode(this, str);
+        Log.i("secret", secret + "");
+        tv.append("加密结果："+secret+"\n");
+        String src = AESEncrypt.decode(this, encode);
+        tv.append("解密结果："+src+"\n");
+        Log.i("src", src + "");
+
+        System.out.println(SignatureTool.getSignature(this));
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+
+    public static native int plus(int a, int b);
+}
